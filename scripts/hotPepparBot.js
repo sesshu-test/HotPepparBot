c84b103f09913364484f947c453d0dfc42fe1aa2
sesshu <ne300153@senshu-u.jp>
2020-08-03 13:52:28 +0900

    検索履歴の表示・消去をできるようにした。手順は「ご飯　地名」でお店を検索したときに、その地名をデータベースに保存。そして、「履歴」または「消去」というワードで、データベースに保存した地名の表示やデータベースにある全てのデータを消去できるようにした。

--- scripts/hotPepparBot.js
+++ scripts/hotPepparBot.js
@@ -6,15 +6,13 @@ const sequelize = new Sequelize(
   'b76c8390159bfa23be1ac2bb1bf4a9640eefd9acb50e52f042f0b14546c8863b',
   {
     //host: 'ホスト名',
-  host: 'c2-35-173-94-156.compute-1.amazonaws.com',
+  host: 'localhost',
   dialect: 'postgres'
   });
 
-const Logs = sequelize.define('messagelogs', {
+const Logs = sequelize.define('searchHistory', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  user_id: Sequelize.TEXT,
-  user_name: Sequelize.TEXT,
-  message: Sequelize.TEXT
+  word: Sequelize.TEXT
   },
   {
     freezeTableName: true // stick to the table name we defined
@@ -25,39 +23,12 @@ sequelize.sync({force: false, alter: true});
 console.log("start");
 
 module.exports = (robot) => {
-  robot.hear(/(.*)$/, async (res) => {
-    console.log("rec");
-    let newLog = new Logs({
-      user_id: res.message.user.id,
-      user_name: res.message.user.displayName,
-      message: res.match[0].slice(6)
-    });
-    await newLog.save();
-  });
 
-  robot.respond(/PRINT (.*)$/i, (res) => {
-    Logs.findAll().then(function(result) {
-      let allMessages = result.map(function(e) {
-        let timeStamp = e.createdAt.toLocaleDateString() + " " + e.createdAt.toLocaleTimeString();
-        if(e.user_name === res.match[1]){
-          return e.user_name + "[" + timeStamp + "]:" + e.message;
-        }else if(e.createdAt.toLocaleDateString() === res.match[1]){
-          return e.user_name + "[" + timeStamp + "]:" + e.message;
-        }
-      });
-      res.send(allMessages.join('\n'));
-    });
-  });
-
-  robot.respond(/CLEAR$/i, async (res) => {
-    await Logs.destroy({ where:{} });
-  });
-
-  robot.respond(/ご飯　(.*)$/i, (res) => {
+  robot.respond(/ご飯　(.*)$/i, async (res) => {
     let location = res.match[1];
     let url = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=6275a5671c376b6a&format=json&order=4&count=3&address=${location}`
     let restaurantData;
-    let info = [];
+    let restaurantInfo = [];
     http.get(url, function(httpResult) {
       let body = '';
       httpResult.on('data', function(chunk) {
@@ -66,14 +37,37 @@ module.exports = (robot) => {
       httpResult.on('end', function(res) {
         restaurantData = JSON.parse(body);
         for(let i = 0; i < 3; i++){
-          info[i] = restaurantData.results.shop[i].name + restaurantData.results.shop[i].urls.pc;
+          restaurantInfo[i] = `${restaurantData.results.shop[i].name}　　　 ${restaurantData.results.shop[i].urls.pc}`;
         }
       });
     });
-    let log = function(){
-      res.send(`${info[0]} \n\n ${info[1]} \n\n ${info[2]}`);
+    let sendInfo = function(){
+      res.send(`${restaurantInfo[0]} \n\n ${restaurantInfo[1]} \n\n ${restaurantInfo[2]}`);
     };
-    setTimeout(log, 1000);
+    setTimeout(sendInfo, 1000);
+    let newSearch = new Logs({
+      word: res.match[1]
+    });
+    await newSearch.save();
+  });
+
+  robot.respond(/履歴$/i, (res) => {
+    Logs.findAll().then(function(result) {
+      let allWords = result.map(function(e) {
+        let timeStamp = e.createdAt.toLocaleDateString() + " "　+ e.createdAt.toLocaleTimeString();
+        return `${e.word} [${timeStamp}]`;
+      });
+      if(allWords.length > 0){
+        res.send("以下のワードで検索しています\n\n" + allWords.join('\n'));
+      }else{
+        res.send("検索履歴はありません");
+      }
+    });
+  });
+
+  robot.respond(/消去$/i, async (res) => {
+    res.send("検索履歴を消去しました");
+    await Logs.destroy({ where:{} });
   });
 
 }
